361f378604fe77111ff4aee940dc64ebc42d9222
Jose Galvez <jjgalper@gmail.com>
2019-06-06 18:53:30 -0500

    Props and children propertie

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
+//Import with Upper case and same name of the file
+//In jsx all element with lowercase are reserver for html elements
+import Person from './Person/Person'
 
+//REACT is all about components
 function App() {
-  //jsx
+  //jsx - > Write html code in js files.
+  //Some restriction like className instead of class, given that class is js reserved word.
+  //JSC expression can olny have one root element
   return (
+    //Use a component inside
     <div className="App">
       <h1>Hi, I'm a new react developer</h1>
+      <p>This is working</p>
+      <Person name="Max" age="30"/>
+      <Person name="Jose" age="25">My hobbies are: soccer </Person>
+      <Person name="Juan" age="21"/>
     </div>
+    //this not
+    //<p>This is working</p>
   );
   //First -> element to render to the DOM
   //Second -> Configuration (javascript object)
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { prototype } from 'module';
+
+//A component is just function which return a jsx
+//Function name lowercase
+
+//Two forms of create a react component
+/*
+ //Functional components (also referred to as "presentational", "dumb" or "stateless" components - more about this later in the course) => 
+ const cmp = () => { return <div>some JSX</div> } (using ES6 arrow functions as shown here is recommended but optional)
+
+ //class-based components (also referred to as "containers", "smart" or "stateful" components) => 
+ class Cmp extends Component { render () { return <div>some JSX</div> } } 
+*/
+
+// To use dinamic content inside jsx {dinamic content}
+// Props are the argument passing throw the function. When the object is render in App.js
+
+//The children propertie is use to go inside the component you want to render.
+const person = (props) => {
+    return (
+        <div>
+            <p>I'm  {props.name} and I have {props.age}</p>
+            <p>{props.children}</p>
+        </div>
+        
+    )
+};
+
+//Export this component to use in main App.js
+export default person;
